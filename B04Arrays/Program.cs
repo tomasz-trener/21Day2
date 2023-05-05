da6003a336985c0cfdf657277a24d72b80f5527d
Tomasz Leś <szkolenia@tomaszles.pl>
2023-05-05 09:53:48 +0200

    Add project files.

---
+++ B04Arrays/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B04Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string city1 = "Warsaw";
+            string city2 = "London";
+            string city3 = "Hongkong";
+            //...
+
+            string[] cities = new string[4];
+
+            cities[0] = city1;
+            cities[1] = city2;
+            // city 2 is missing 
+            cities[3] = "Singapour";
+
+            cities[1] = "Berlin";
+            
+           // cities[4] = "Paris"; // index out of range exception 
+
+            int arrayLength = cities.Length; // 4 
+
+            Console.WriteLine(cities[0]);
+            Console.WriteLine(cities[2]);
+
+
+            int[] numbers = new int[2];
+            Console.WriteLine(numbers[0]);
+
+            bool[] boolArray = new bool[2];
+
+
+            int?[] numbers1 = new int?[2];
+
+            int[] numbers2 = new int[3] { 4, 5, 7 };
+
+
+            int[] numbers3 = { 4, 5, 7 };
+
+            bool?[] boolArray2 = { true, false, true };
+
+            char[] characters = { 'a', 'b', 'c' };
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ B05Ex_Arrays/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B05Ex_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // char onlyOneCharacter = 'a';
+
+            string searchChar = "°";
+            string endChar = ">";
+            const string path = @"cities.txt";
+
+
+            string[] citiesFromFile = File.ReadAllLines(path);
+
+            Console.WriteLine("Avaible cities:");
+            for (int i = 0; i < citiesFromFile.Length; i++)
+                Console.WriteLine($"[{i + 1}] {citiesFromFile[i]}");
+
+            while (true)
+            {
+                //Console.WriteLine("Enter the city name");
+                // string city = Console.ReadLine();
+                Console.WriteLine("select a city (enter the number)");
+                string cityString = Console.ReadLine();
+                int cityNumber = Convert.ToInt32(cityString);
+                string city = citiesFromFile[cityNumber - 1];
+
+
+                string address = $"https://www.google.com/search?q=weather+{city}";
+
+                WebClient wc = new WebClient();
+                string data = wc.DownloadString(address);
+
+                try
+                {
+                    int index = data.IndexOf(searchChar);
+                    int currentPosition = index;
+                    int iterationCount = 0;
+
+                    while (data.Substring(currentPosition, 1) != endChar)
+                    {
+                        iterationCount++;
+                        currentPosition--;
+                    }
+
+                    string result = data.Substring(currentPosition + 1, index - currentPosition + 1);
+                    Console.WriteLine(result);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Faild to retrieve temperature");
+                    continue;
+                }
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
